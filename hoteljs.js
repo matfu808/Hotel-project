29adb217caee90b4c98eabf1333f45b8a50c1cb2
matfu808 <56744341+matfu808@users.noreply.github.com>
2019-10-25 14:23:17 -1000

    10-25/19

---
+++ hoteljs.js
@@ -0,0 +1,52 @@
+var p1r1 = "";
+var p2r1 = "";
+var p3r1 = "";
+var p4r1 = "";
+var p1r2 = "";
+var p2r2 = "";
+var p3r2 = "";
+var p4r2 = "";
+var p2r3 = "";
+var p3r3 = "";
+var p4r3 = "";
+var p1r3 = "";
+var p2r4 = "";
+var p3r4 = "";
+var p4r4 = "";
+var p1r4 = "";
+var p2r5 = "";
+var p3r5 = "";
+var p4r5 = "";
+var p1r5 = "";
+var p2r6 = "";
+var p3r6 = "";
+var p4r6 = "";
+var p1r6 = "";
+
+
+
+function addroomnumber() {
+  var inputroomnumber = document.getElementById("roomnumber").value;
+  if (inputroomnumber == "1") {
+
+  } 
+  else if (inputroomnumber == "2") {
+
+  } 
+  else if (inputroomnumber == "3") {
+
+  } 
+  else if (inputroomnumber == "4") {
+
+  } 
+  else if (inputroomnumber == "5") {
+
+  } 
+  else if (inputroomnumber == "6") {
+
+  }
+  else {
+      windows.alert("please choose a valid room number from 1-6")
+  }
+}
+
